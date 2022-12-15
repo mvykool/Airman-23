e93d195933bcc29b0139871e54b22aaa85cbea1e
maicol <maykolandres4@gmail.com>
2022-12-15 06:52:28 -0500

    added schemas to start building the blog

--- airman-23/sanity-project/schemas/index.js
+++ airman-23/sanity-project/schemas/index.js
@@ -1,3 +1,7 @@
 import product from "./product"
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
 
-export const schemaTypes = [product]
+export const schemaTypes = [product, post, author, category, blockContent]
