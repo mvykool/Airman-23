f9a8f5ca553347613bb96fa07a7aa81257a0e7aa
maicol <maykolandres4@gmail.com>
2022-12-17 12:16:00 -0500

    fixed tialwind issue

--- airman-23/components/Banner.tsx
+++ airman-23/components/Banner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='bg-red-500 m-8 p-5 rounded-xl'>
+    <div className='bg-red-600 m-8 p-5 rounded-xl'>
        <p className='flex justify-center text-white font-semibold'>New Products!</p>
        <div className='flex justify-center m-3'>
          <h1 className='font-bold text-white text-2xl'>Winter 2022</h1>
