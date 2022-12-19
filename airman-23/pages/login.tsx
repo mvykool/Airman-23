878a7a04965ec85d6e920c5fb9b1c0ea449fb4c4
maicol <maykolandres4@gmail.com>
2022-12-18 22:08:01 -0500

    edited login page

--- airman-23/pages/login.tsx
+++ airman-23/pages/login.tsx
@@ -33,17 +33,19 @@ if(user) router.push({ pathname: '/' })
 return (
 <>
 
-<div className='bg-app h-screen pt-10'>
+<div className='bg-gray-200 pt-10 pb-44 login'>
 <Head>
    <title>IG-CLONE | log in</title>
    
 </Head>
-     <div className='grid place-items-center p-10 mx-[20%] xl:py-20 xl:px-10 xl:mx-[40%] login-card bg-white mt-36 rounded-md shadow-lg'>
- 
+     <div className='grid place-items-center py-14 px-10 mx-[20%] xl:py-10 xl:px-10 xl:mx-[40%] login-card bg-white mt-36 rounded-md shadow-lg'>
 
-       <div className='mt-5 flex-col flex'>
-       <button type='button' onClick={googleSignIn}  className='mt-20 cursor-pointer p-3 xl:p-5  bg-black hover:scale-110 transition duration-100 hover:bg-gray-900 rounded-xl text-white text-center'>Login with Google</button>
-       <button type='button' onClick={signInAsGuess}  className='mt-10 cursor-pointer p-2 xl:p-4  bg-app hover:scale-110 transition duration-100 hover:bg-gray-200 rounded-xl text-blackz text-center'>Login as demo</button>
+      <h1 className='font-bold text-xl text-[#00708c]'>Airman-23</h1>
+
+       <div className='flex-col flex'>
+       <button type='button' onClick={googleSignIn}  className='mt-10 cursor-pointer p-3 xl:p-5  bg-[#00708c] hover:scale-110 transition duration-100 hover:bg-gray-900 rounded-xl text-white font-semibold text-center'>Login with Google</button>
+       <button type='button' onClick={signInAsGuess}  className='mt-10 cursor-pointer p-2 xl:p-4  bg-app hover:scale-110 transition duration-100 hover:bg-gray-200 rounded-xl text-[#00708c] border-2 border-[#00708c] text-center font-semibold'>Login as demo</button>
+
   </div>
 </div>
   </div>
