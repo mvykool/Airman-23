19957cc98ff24421a9429b06a06a3fbddab2f083
maicol <maykolandres4@gmail.com>
2022-12-18 23:01:04 -0500

    collection edited

--- airman-23/pages/collection/[slug].tsx
+++ airman-23/pages/collection/[slug].tsx
@@ -6,6 +6,7 @@ import { useStateContext } from '../../context/StateContext'
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { client, urlFor } from '../../lib/client'
+import { MdOutlineKeyboardBackspace } from 'react-icons/md';
 
 
 interface Props{
@@ -33,13 +34,13 @@ const ProductDetails = ({ product, products }: Props) => {
   const router = useRouter()
 
   const goBack = () => {
-    router.push('..')
+    router.back( )
   }
 
   console.log(product)
 
   return (
-    <div className='mt-20'>
+    <div className='bg-gray-200 pb-10'>
   
     <Head>
       <title>Air man 23 | details</title>
@@ -47,55 +48,45 @@ const ProductDetails = ({ product, products }: Props) => {
 
 
         <div>
-
-            <div>
+            <div className='bg-[#00708c] py-10'>
               
-              <div onClick={goBack} >GO back</div>
+            <div className='mt-10 bg-white rounded-full h-10 w-10 flex justify-center items-center ml-8'>
+            <MdOutlineKeyboardBackspace className='text-xl' onClick={goBack}/>
+             </div>
 
-                <div className='image-container'>
-                  <Image width={60} height={60} alt='product img' src={urlFor(image && image[index]).url()} className='product-detail-image'/>
+                <div className='flex justify-center'>
+                  <Image width={220} height={220} alt='product img' src={urlFor(image && image[index]).url()}/>
                 </div>
             </div>
      
 
            
-           <div className="product-detail-desc">
-          <h1>{name}</h1>
-          <div className="reviews">
-            <div>
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiOutlineStar />
-            </div>
-            <p>
-              (20)
-            </p>
-          </div>
-          <h4>Details: </h4>
-          <p>{details}</p>
-          <p className="price">${price}</p>
-          <div className="quantity">
-            <h3>Quantity:</h3>
-            <p className="quantity-desc">
-              <span className="minus" onClick={decQty} ><AiOutlineMinus /></span>
+           <div className="mx-8">
+          <h1 className='my-5 text-2xl font-bold text-[#00708c]'>{name}</h1>
+        
+          <h4 className='border-b-4 border-[#00708c] w-14 my-5 font-semibold'>Details</h4>
+          <p className='my-3'>{details}</p>
+          <p className="font-bold text-2xl my-4"><span className='text-green-600'>$</span>{price}</p>
+          <div >
+            <h3 className='my-5 font-semibold border-b-4 border-[#00708c] w-20'>Quantity</h3>
+            <p className="my-6 flex space-x-3 items-center">
+              <span className="text-white bg-black p-1 rounded-sm"  onClick={decQty} ><AiOutlineMinus /></span>
               <span className="num">{qty}</span>
-              <span className="plus" onClick={incQty} ><AiOutlinePlus /></span>
+              <span className="text-white bg-black p-1 rounded-sm"  onClick={incQty} ><AiOutlinePlus /></span>
             </p>
           </div>
-          <div className="buttons">
-            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
-            <button type="button" className="buy-now" onClick={handleBuyNow} >Buy Now</button>
+          <div className="flex space-x-4 my-10">
+            <button type="button" className="p-2 rounded-md border-2 border-[#00708c] shadow-sm bg-white font-semibold text-[#00708c]" onClick={() => onAdd(product, qty)}>Add to Cart</button>
+            <button type="button" className="p-2 rounded-md bg-[#00708c] text-white font-semibold shadow-md" onClick={handleBuyNow} >Buy Now</button>
           </div>
         </div>
       </div>
 
 
-      <div className='maylike-products-wrapper'>
-        <h2>You may also like</h2>
-        <div className='marquee'>
-            <div className='maylike-products-container track'>
+      <div className='mx-8'>
+        <h2 className='border-b-4 border-[#00708c] w-36 font-semibold'>You may also like</h2>
+        <div>
+            <div className='flex my-5'>
                {products.slice(0,2).map((item: any) => (
                 <Product key={item._id} product={item} image={undefined} name={''} price={0} slug={''} />
                ))} 
