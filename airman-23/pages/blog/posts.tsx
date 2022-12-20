0ba719c172426a303c117b6286bc785e7781e99a
maicol <maykolandres4@gmail.com>
2022-12-19 22:06:09 -0500

    edited blog posts UI

--- airman-23/pages/blog/posts.tsx
+++ airman-23/pages/blog/posts.tsx
@@ -1,6 +1,8 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
+import { MdOutlineKeyboardBackspace } from 'react-icons/md'
 import { client } from '../../lib/client'
 
 interface postsProps {
@@ -18,10 +20,19 @@ const blogPost = ({posts}: postsProps) => {
   console.log(posts)
 
   return (
-    <div>blogPost
+    <div className='pt-10 bg-gray-200 pb-10'>
 
-    <div  className='mt-20 ml-5' onClick={goBack}>Go back</div>
-     <h1 className='mt-20'> Blog</h1>
+    <Head>
+      <title>Air man 23 | Blog</title>
+    </Head>   
+
+    <div className='mt-10 bg-white shadow-md rounded-full h-10 w-10 flex justify-center items-center ml-8'>
+      <MdOutlineKeyboardBackspace className='text-xl' onClick={goBack}/>
+    </div>
+
+    <div className='flex justify-center mx-8 p-5 rounded-lg my-10 bg-[#00708C]'>
+      <h1 className='font-bold text-white text-2xl'>Blog</h1>
+      </div>
 
 
       <div>
