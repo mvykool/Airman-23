63373ef7170e0ca1364d989a4fa776dccbce73d3
maicol <maykolandres4@gmail.com>
2022-12-20 20:51:11 -0500

    installed sendgrid

--- airman-23/components/Hero.tsx
+++ airman-23/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
 
          <div className='pt-16 flex justify-center'>
          <Link href={'/shop'} >
-         <p className='text-white text-lg border-b-2'>shop now</p>
+         <p className='text-white text-lg border-b-2'>Go to shop</p>
          </Link>
        </div>
          </div>
