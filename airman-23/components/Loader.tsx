cc3c2375118c90767957611c16256d64314017e8
maicol <maykolandres4@gmail.com>
2022-12-23 10:34:24 -0500

    added loader

---
+++ airman-23/components/Loader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image';
+
+const Loader = () => {
+  return (
+    <div className='h-[100%] pt-80 bg-[var(--bg-loader)] w-full fixed'>
+        <div className='flex justify-center items-center'>
+        <Image  alt='loader' width={100} height={100} src='/Loader.svg' />
+        </div>
+    </div>
+  )
+}
+
+export default Loader
--- airman-23/pages/_app.tsx
+++ airman-23/pages/_app.tsx
@@ -1,12 +1,30 @@
 import '../styles/globals.css'
+import React, { useState } from 'react'
 import type { AppProps } from 'next/app'
 import  Layout  from '../components/Layout'
 import { StateContext } from '../context/StateContext'
+import Router from 'next/router'
+import Loader from '../components/Loader';
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  //Router event loading
+  const [loading, setLoading] = useState(false)
+
+  Router.events.on('routeChangeStart', (url) => {
+    setLoading(true)
+  })
+
+  Router.events.on('routeChangeComplete', (url) => {
+    setLoading(false)
+  })
+
   return (
     <StateContext>
     <Layout>
+      { loading &&  (
+        <Loader/>
+      )}
       <Component {...pageProps} />
     </Layout>
     </StateContext>
