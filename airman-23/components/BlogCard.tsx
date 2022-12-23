e7df98d8a115e9c561b78f1b3da853e4eec05bb4
maicol <maykolandres4@gmail.com>
2022-12-23 00:15:12 -0500

    changed link to div

--- airman-23/components/BlogCard.tsx
+++ airman-23/components/BlogCard.tsx
@@ -10,7 +10,7 @@ interface Props{
 const BlogCard = ({post}: Props) => {
   return (
     <div className='py-10'>
-    <Link href={`/blog/${post.slug.current}`} className='my-2 relative'>
+    <div className='my-2 relative'>
       
       <p className=' p-4 font-bold absolute text-white bg-[var(--bg-wrapper)] w-full h-full rounded-md'>{post.title}</p>
 
@@ -21,9 +21,9 @@ const BlogCard = ({post}: Props) => {
         src={urlFor(post.mainImage.asset).url()}
         className='h-52 w-52 rounded-md'
         /> 
-     <button className='text-xs font-semibold bg-white absolute -mt-14 ml-3 p-2 rounded-xl'>Read more</button>
+     <Link href={`/blog/${post.slug.current}`}  className='text-xs font-semibold bg-white absolute -mt-14 ml-3 p-2 rounded-xl'>Read more</Link>
 
-    </Link>
+    </div>
 
 
   </div>
