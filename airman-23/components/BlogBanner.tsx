33fc1851aa1167a6a9b8aafc2c4cd6b299330ddc
maicol <maykolandres4@gmail.com>
2022-12-22 23:18:55 -0500

    removed underline

--- airman-23/components/BlogBanner.tsx
+++ airman-23/components/BlogBanner.tsx
@@ -4,7 +4,7 @@ const BlogBanner = () => {
   return (
     <div className='bg-[#00708C] mx-8 my-14 p-5 rounded-lg'>
     <div className='flex justify-center m-3'>
-      <h1 className='font-bold text-white text-lg underline'>What's new in the blog?</h1>
+      <h1 className='font-bold text-white text-lg'>What's new in the blog?</h1>
     </div>
   </div>
   )
