6d568b83cf54b8995a9955d9d090e1913c27092b
maicol <maykolandres4@gmail.com>
2022-12-31 03:58:11 -0500

    edited logo

--- airman-23/components/Navbar.tsx
+++ airman-23/components/Navbar.tsx
@@ -69,7 +69,7 @@ if (typeof window !== "undefined") {
          <div className='mx-2'>
            <Link href={'/'} onClick={() => setOpenMenu(false)}>
             <div className='md:flex md:items-center md:space-x-3'>
-             <p className='hidden md:block md:text-3xl logo-font'>Air man-23</p> 
+             <p className='hidden md:block md:text-3xl text-gray-500 logo-font'>Air man-23</p> 
             <Image src={'/fireball.png'} alt='logo' width={35} height={35} className='md:h-10 md:w-10'  />
             </div>
        </Link>
